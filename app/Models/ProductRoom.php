67844022cb963d18c5492f645fab50b69179cb5e
phamminh12 <aeonepiece7a4@gmail.com>
2024-07-27 21:52:26 +0700

    Add product_rooms table and edit rooms table

---
+++ app/Models/ProductRoom.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Product
+ * 
+ * @property int $id
+ * @property int $amount
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * 
+ * @property Product $product
+ * @property Room $room
+ *
+ * @package App\Models
+ */
+class ProductRoom extends Model
+{
+    protected $table = 'product_rooms';
+
+    protected $fillable = [
+        'product_id',
+        'room_id',
+        'amount'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    public function room()
+    {
+        return $this->belongsTo(Room::class);
+    }
+}
