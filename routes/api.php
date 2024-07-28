c002b37b703e7d93e151a06143237f00d5f9952c
phamminh12 <aeonepiece7a4@gmail.com>
2024-07-29 06:33:33 +0700

    Adding customer controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Routing\RouteUri;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\StaffController;
+use App\Http\Controllers\CustomerController;
 
 
 Route::get('/hello', function(){
@@ -13,16 +14,34 @@ Route::get('/hello', function(){
 
 Route::post('/abc', [StaffController::class, 'create']);
 
-Route::get('/rooms', [RoomController::class, 'index']);
-Route::get('/rooms/{id}', [RoomController::class, 'show']);
-Route::post('/rooms/create', [RoomController::class, 'store']);
-Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
-Route::put('/rooms/{id}', [RoomController::class, 'update']);
+Route::controller(RoomController::class)->group(function () {
+  Route::get('/rooms', 'index');
+  Route::get('/rooms{id}', 'show');
+  Route::post('/rooms/create', 'store');
+  Route::delete('/rooms/{id}', 'destroy');
+  Route::put('/rooms/{id}', 'update');
+});
+// Route::get('/rooms', [RoomController::class, 'index']);
+// Route::get('/rooms/{id}', [RoomController::class, 'show']);
+// Route::post('/rooms/create', [RoomController::class, 'store']);
+// Route::delete('/rooms/{id}', [RoomController::class, 'destroy']);
+// Route::put('/rooms/{id}', [RoomController::class, 'update']);
 
-Route:: get('/staff', [StaffController::class, 'index']);
-Route:: post('/staff', [StaffController::class, 'store']);
-Route:: delete('/staff/{id}', [StaffController::class, 'destroy']);
-Route:: get('/staff/{id}', [StaffController::class, 'show']);
-Route:: put('/staff/{id}', [StaffController::class, 'updateProfile']);
-Route:: put('/staff-email/{id}', [StaffController::class, 'accessEmail']);
+Route::controller(StaffController::class)->group(function () {
+  Route::get('/staff', 'index');
+  Route::post('/staff', 'store');
+  Route::delete('/staff/{id}', 'destroy');
+  Route::get('/staff/{id}', 'show');
+  Route::put('/staff/{id}', 'updateProfile');
+  Route::put('/staff-email/{id}', 'accessEmail');
+});
+// Route:: get('/staff', [StaffController::class, 'index']);
+// Route:: post('/staff', [StaffController::class, 'store']);
+// Route:: delete('/staff/{id}', [StaffController::class, 'destroy']);
+// Route:: get('/staff/{id}', [StaffController::class, 'show']);
+// Route:: put('/staff/{id}', [StaffController::class, 'updateProfile']);
+// Route:: put('/staff-email/{id}', [StaffController::class, 'accessEmail']);
 
+Route::controller(CustomerController::class)->group(function () {
+  Route::get('/customers', 'index');
+});
